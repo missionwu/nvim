2bf2e64955cbd4371a70f866f12e0928740bbabc
Mission <mission_wu@outlook.com>
2025-03-07 22:59:02 +0800

    init

---
+++ lua/plugins/escape.lua
@@ -0,0 +1,10 @@
+return {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+        require("better_escape").setup {
+            timeout = vim.o.timeoutlen,
+            default_mappings = true,
+        }
+    end,
+}
