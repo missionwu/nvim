5ff96aa12505434e511af86ad27919b963d68877
Mission <mission_wu@outlook.com>
2026-05-10 19:11:28 +0800

    update: disable StayCentered in neo-tree

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -10,6 +10,10 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd({ "CursorMovedI", "InsertEnter" }, {
     group = vim.api.nvim_create_augroup("StayCentered", { clear = true }),
     callback = function()
+        if vim.bo.filetype == "neo-tree" then
+            return
+        end
+
         local line = vim.api.nvim_win_get_cursor(0)[1]
         if vim.b.last_line == nil or line ~= vim.b.last_line then
             local column = vim.fn.getcurpos()[3]
